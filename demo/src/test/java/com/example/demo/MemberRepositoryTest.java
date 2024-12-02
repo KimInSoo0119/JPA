bf7df3e63c107433a5da34bf372583db88ca599d
soong <soong@DESKTOP-R3OEBIU>
2024-12-02 23:15:05 +0900

    testcase 작성 및 h2 db 세팅

---
+++ demo/src/test/java/com/example/demo/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+	@Autowired MemberRepository memberRepository;
+	
+	@Test
+	@Transactional 
+	/*EntityManager를 통한 모든 데이터변경은 transaction 안에서 이루어져야 한다. 여기 말고 MemberRepository 선언해도됨*/
+	public void testMember() throws Exception {
+		//given
+		Member member = new Member();
+		member.setUsername("memberA");
+		
+		//when
+		Long saveId = memberRepository.save(member);
+		Member findMember = memberRepository.find(saveId);
+		
+		//then
+		Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+		Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+	}
+}
