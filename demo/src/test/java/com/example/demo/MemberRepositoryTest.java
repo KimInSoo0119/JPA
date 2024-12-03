6e52976c50bed333b0dcff10d233325cdc5e7b73
soong <soong@DESKTOP-R3OEBIU>
2024-12-03 21:31:15 +0900

    테스트케이스 작성 및 DB동작확인

--- demo/src/test/java/com/example/demo/MemberRepositoryTest.java
+++ demo/src/test/java/com/example/demo/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +16,10 @@ class MemberRepositoryTest {
 	@Autowired MemberRepository memberRepository;
 	
 	@Test
-	@Transactional 
+	@Transactional
+	@Rollback(false)
 	/*EntityManager를 통한 모든 데이터변경은 transaction 안에서 이루어져야 한다. 여기 말고 MemberRepository 선언해도됨*/
+	/*Transactional이 테스트케이스에 있을경우 트랜잭션이후, 롤백*/
 	public void testMember() throws Exception {
 		//given
 		Member member = new Member();
@@ -29,5 +32,7 @@ class MemberRepositoryTest {
 		//then
 		Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
 		Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+		/*같은 트랜잭션 즉, 영속성 컨텍스트에 저장되어있음, 같은 엔티티*/
+		Assertions.assertThat(findMember).isEqualTo(member); 
 	}
 }
